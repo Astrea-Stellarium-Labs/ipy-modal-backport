aa70ff1399d40f53a90c861e13373b6c91f2f32c
Astrea <25420078+AstreaTSS@users.noreply.github.com>
2026-01-24 16:19:27 -0500

    chore: bump version

--- modal_backport/__init__.py
+++ modal_backport/__init__.py
@@ -6,7 +6,7 @@ A project to backport new modal features into interactions.py v5.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 from .components import *
 from .context import *
